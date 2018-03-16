1e3809885fc4e6cc077f8945dcecf2a7fc942d0e
george <gkosmo1@hotmail.com>
2018-03-16 18:08:10 +0100

    ekoerz

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,6 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
- before_action :check_cluster
+ before_action :check_cluster, except: [:new]
   # GET /profiles
   # GET /profiles.json
   def index
