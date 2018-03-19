8a8714a20fc474b3141bbd4ba017229a2a2aabbc
george <gkosmo1@hotmail.com>
2018-03-19 13:47:08 +0100

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,6 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-config.assets.js_compressor = :uglifier
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
@@ -28,7 +27,7 @@ config.assets.js_compressor = :uglifier
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
